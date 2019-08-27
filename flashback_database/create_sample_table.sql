c08cd949f24ceabc92d9a4a936f089e4d69fa397
Jerry Wang <j3rry.wan9@gmail.com>
2019-08-27 11:42:46 -0700

    use a sequence for the scn_id column

--- flashback_database/create_sample_table.sql
+++ flashback_database/create_sample_table.sql
@@ -1,27 +1,29 @@
 -- Create a table for Flashback Database verification
+drop sequence scn_id_seq;
+
+create sequence scn_id_seq nocycle nocache;
+
+drop table sample purge;
+
+create table sample(scn_id number, current_scn number);
+
 set serveroutput on;
 
 declare
+    scn_id number;
     current_scn number;
 begin
---    execute immediate 'drop table sample purge';
---exception
---     when others then
---         if sqlcode != -0942 then raise;
---         end if;
-
---    execute immediate 'create table sample(id number, current_scn number)';
-
-    for i in 21..30 loop
+    for i in 1..10 loop
+        select scn_id_seq.nextval into scn_id from dual;
         select current_scn into current_scn from v$database;
-        dbms_output.put_line(i);
+        dbms_output.put_line(scn_id);
         dbms_output.put_line(current_scn);
-        execute immediate 'insert into sample values(:1, :2)' using i, current_scn;
+        execute immediate 'insert into sample values(:1, :2)' using scn_id, current_scn;
         execute immediate 'alter system switch logfile';
     end loop;
 
     FOR cursor1 IN (SELECT * FROM sample)
         LOOP
-            DBMS_OUTPUT.PUT_LINE('id = ' || cursor1.id || ', current_scn = ' || cursor1.current_scn);
+            DBMS_OUTPUT.PUT_LINE('id = ' || cursor1.scn_id || ', current_scn = ' || cursor1.current_scn);
         END LOOP;
 end;
