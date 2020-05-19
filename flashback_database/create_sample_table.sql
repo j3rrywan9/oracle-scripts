50e7c69c98ac576a59d7fdde80b8c805c902196f
Jerry Wang <j3rry.wan9@gmail.com>
2020-05-19 10:00:08 -0700

    update flashback_database/create_sample_table.sql

--- flashback_database/create_sample_table.sql
+++ flashback_database/create_sample_table.sql
@@ -1,18 +1,27 @@
 -- Create a table for Flashback Database verification
-drop sequence scn_id_seq;
 
-create sequence scn_id_seq nocycle nocache;
+--create sequence scn_id_seq nocycle nocache;
 
-drop table sample purge;
+--drop table sample purge;
 
-create table sample(scn_id number, current_scn number);
+--create table sample(scn_id number, current_scn number);
 
 set serveroutput on;
 
 declare
+    sequence_does_not_exist exception;
+    pragma exception_init(sequence_does_not_exist, -2289);
+    table_or_view_does_not_exist exception;
+    pragma exception_init(table_or_view_does_not_exist, -942);
     scn_id number;
     current_scn number;
 begin
+    execute immediate 'drop sequence scn_id_seq';
+    execute immediate 'create sequence scn_id_seq nocycle nocache';
+
+    execute immediate 'drop table sample purge';
+    execute immediate 'create table sample(scn_id number, current_scn number)';
+
     for i in 1..10 loop
         select scn_id_seq.nextval into scn_id from dual;
         select current_scn into current_scn from v$database;
@@ -26,4 +35,8 @@ begin
         LOOP
             DBMS_OUTPUT.PUT_LINE('id = ' || cursor1.scn_id || ', current_scn = ' || cursor1.current_scn);
         END LOOP;
+
+exception
+    when sequence_does_not_exist or table_or_view_does_not_exist then
+        null;
 end;
